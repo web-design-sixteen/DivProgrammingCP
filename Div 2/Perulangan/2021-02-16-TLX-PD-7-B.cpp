e50798187c5758b265938c166a5efd4e9425149a
Elrond <59683978+ElrondRafsaAdjani@users.noreply.github.com>
2021-03-05 15:03:42 +0700

    Create 2021-02-16-TLX-PD-7-B.cpp

---
+++ Div 2/Perulangan/2021-02-16-TLX-PD-7-B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	for(int i=n;i>0;i--){
+		for(int j=1;j<=n;j++){
+			if(j<i) cout << " ";
+			else cout << "*";
+		}
+		
+		cout << endl;
+	}
+	
+}
