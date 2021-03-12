98e6b66aee987b4c899af344ddf2fd23b00e1bb2
Elrond <59683978+ElrondRafsaAdjani@users.noreply.github.com>
2021-03-12 14:16:39 +0700

    Update 2021-03-05-TLX-PD-7-B.cpp

--- Div 2/Perulangan/2021-03-05-TLX-PD-7-B.cpp
+++ Div 2/Perulangan/2021-03-05-TLX-PD-7-B.cpp
@@ -1,3 +1,4 @@
+// SOLUSI 1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,3 +15,37 @@ int main(){
 	}
 	
 }
+
+// SOLUSI 2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >>n;
+	for(int i=1;i<=n;i++){
+		// perulagan untuk mengeluarkan spasi
+		for(int j=1;j<=(n-i);j++){
+			cout << " ";
+		}
+		// perulangan untuk mengeluarkan bintang
+		for(int j=1;j<=i;j++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
+
+// clue : nested loop (perulangan dalam perulangan)
+
+// n = 5
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+
+
+
