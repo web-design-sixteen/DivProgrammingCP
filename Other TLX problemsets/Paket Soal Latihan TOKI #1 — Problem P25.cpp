394711e9a5fd7ff54916628fe8b3c1675636d947
Elrond <59683978+ElrondRafsaAdjani@users.noreply.github.com>
2021-05-04 14:34:51 +0700

    Create Paket Soal Latihan TOKI #1 — Problem P25.cpp

---
+++ Other TLX problemsets/Paket Soal Latihan TOKI #1 — Problem P25.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,a,ada[100005]={0};
+	cin >> n;
+	FOR(i,1,n-2){
+		cin >> a;
+		ada[a] = 1;
+	} 
+	// FOR(i,1,n-2) cout << i << " " << ada[i] << endl;
+	FOR(i,1,n){
+		if(ada[i]==0) cout << i << endl;
+	}
+
+}
