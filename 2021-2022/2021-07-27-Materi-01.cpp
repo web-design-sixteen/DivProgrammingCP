388aa15ace5e0727ce6cfa796d9a2801366cfc00
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:21:24 +0700

    Update 2021-07-27-Materi-01.cpp

--- 2021-2022/2021-07-27-Materi-01.cpp
+++ 2021-2022/2021-07-27-Materi-01.cpp
@@ -12,3 +12,14 @@ int main(){
 	cout << endl;
 	cout << katakan_halo << endl;
 }
+
+// syntax,command mengeluarkan output : cout <<
+// syntax,command membuat baris baru : endl
+
+/*
+tipe data untuk kata/kalimat : string
+tipe data untuk bilangan bulat : int
+tipe data untuk bilangan riil
+tipe data karakter
+tipe data boolean
+*/
