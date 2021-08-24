fd930ca48e3f40f1fd8c644d3a1c56044a8b81e7
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:25:26 +0700

    Create 2021-08-03-TLX-PD-1-A.cpp

---
+++ 2021-2022/2021-08-03-TLX-PD-1-A.cpp
@@ -0,0 +1,13 @@
+// Pembahasan Problem A Perkenalan
+
+#include <bits/stdc++.h>
+#include <cstdio>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cout << "Halo, dunia!" << endl;
+}
