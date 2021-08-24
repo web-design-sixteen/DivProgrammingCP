b21b443f787a047cfc0a02aaabed33cabe1c0ddf
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:32:21 +0700

    Create 2021-08-03-Materi-2.cpp

---
+++ 2021-2022/2021-08-03-Materi-2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // perintah mengambil input dipisahkan oleh spasi : cin >>
+ // perintah mengambil input satu baris : getline(cin, <nama variabel>)
+int main(){
+   string kalimat;
+   getline(cin, kalimat);
+   cout << kalimat << endl;
+
+}
+
+// tipe data bilangan bulat : int
+// tipe data bilangan riil :
+// tipe data kata/kalimat : string
