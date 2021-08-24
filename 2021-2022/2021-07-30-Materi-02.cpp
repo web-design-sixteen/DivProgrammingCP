2cba379bad3085005721380c2c60475072aaa541
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:22:19 +0700

    Create 2021-07-30-Materi-02.cpp

---
+++ 2021-2022/2021-07-30-Materi-02.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// comment adalah bagian kode yang tidak dieksekusi komputer
+	// <tipe data> <nama variabel> <=> <nilai>;
+	
+	// operasi aritmatika : 
+
+	// penjumlahan (+)
+	// pengurangan (-)
+	// perkalian (*)
+	// pembagian, div (/)
+	// modulus (%)
+
+
+	// pembulatan ke bawah hasil pembagian
+	5 / 2 = 2
+	13 / 4 = 3
+	24 / 5 = 4
+
+	// modulus -> sisa pembagian
+	5 / 2 = 2 * 2 = 4 sisa 1
+	13 / 4 = 4 * 3 = 12 sisa 1
+	24 / 5 = 5 * 4 = 20 sisa 4
+
+	5 % 2 = 1
+	13 % 4 = 1
+	24 % 5 = 4
+	
+}
+
+
+
+// syntax,command mengeluarkan output : cout << 
+// syntax,command membuat baris baru : endl
+
+/*
+tipe data untuk kata/kalimat : string
+tipe data untuk bilangan bulat : int
+tipe data untuk bilangan riil : double
+tipe data karakter
+tipe data boolean
+*/
