57f1be8bacc7404d47f1602d005594d7a4e7a5e5
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:34:31 +0700

    Create 2021-08-13-Materi.cpp

---
+++ 2021-2022/2021-08-13-Materi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	//tipe data boolean (bool) : true, false
+	// operasi logika : and (dan), or (atau)
+	// true AND true = true
+	// true AND false = false
+	// false AND true = false
+	// false AND false = false
+	// true OR false = true
+	// true OR true = true
+	// false OR false = false
+	// false OR true = true
+	
+	// jika pernyatan di dalam kurung itu benar, maka jalankah perintah
+	// yang ada di dalam kurung kurawa
+	int a, b;
+	cin >> a >> b;
+	if(a < b){
+		cout << "iya benar, bahwa " << a << " itu kurang dari " << b << endl;
+	}
+}
