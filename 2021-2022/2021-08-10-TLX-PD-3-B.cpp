1f832af50d02b75d0720e6c29761d2b7b216fb70
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:33:52 +0700

    Create 2021-08-10-TLX-PD-3-B.cpp

---
+++ 2021-2022/2021-08-10-TLX-PD-3-B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// menyediakan variabel untuk menampung sebuah kalimat
+	// mengambil input untuk variabel tersebut
+	// mencetak variabel tersebut
+	// getline(cin, <nama variabel>)  
+	string kalimat;
+	getline(cin, kalimat);
+	cout << kalimat << endl;
+	
+	/*
+	bilangan bulat : int
+	bilangan riil : double
+	kata / kalimat : string
+	boolean true / false : bool
+	karakter : char
+	*/
+	
+}
