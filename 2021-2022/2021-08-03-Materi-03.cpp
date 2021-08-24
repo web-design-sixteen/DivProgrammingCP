be555bdcd19e3581a9b4e8547089f12a71893a69
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:26:05 +0700

    Create 2021-08-03-Materi-03.cpp

---
+++ 2021-2022/2021-08-03-Materi-03.cpp
@@ -0,0 +1,44 @@
+// header
+#include <bits/stdc++.h>
+using namespace std;
+
+// fungsi utama
+
+// perintah pertama : cetak "halo dunia, halo"
+// perintah kedua : cetak baris baru (endl)
+// perintah ketiga : cetak "umurku 17 tahun"
+
+int main(){
+	cout << "halo dunia, halo" << endl;
+	cout << "umurku 17 tahun" << endl;
+
+	// <tipe data> <nama variabel> = <nilai>;
+	int angka = 15;
+	double random = 3.14;
+
+	cout << angka << endl;
+	cout << random << endl;
+
+	// penjumlahan (+)
+	// pengurangan (-)
+	// pengalian (*)
+	// pembagian, div (/)
+	// modulus (%)
+
+	// div : pembulatan ke-bawah hasil pembagian atau faktor terdekat mendekati pembilang
+
+	// modulus : sisa hasil bagi
+
+	cout << 5 + 16 << endl;
+	cout << 235 * 123 << endl;
+	cout << 100000007 % 9 << endl;
+
+}
+
+// tipe data 
+
+// 1. bilangan bulat : integer = int
+// 2. bilangan riil : floating = float, double 
+// 3. karakter
+// 4. kata atau kalimat
+// 5. boolean
