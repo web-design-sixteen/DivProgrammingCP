1fe348d00794a42b71a0ebc9d89325b8797aa85d
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:19:56 +0700

    Create 2021-07-27-Materi-11.cpp

---
+++ 2021-2022/2021-07-27-Materi-11.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// <tipe data> <nama variabel> = <nilai>
+	string katakan_halo = "halo dunia";
+
+	int tepak = 5;
+	cout << "sekarang nilai tepaknya : " << tepak << endl;
+	tepak = 6;
+	cout << "sekarang nilai tepaknya : " << tepak << endl;
+	cout << endl;
+	cout << katakan_halo << endl;
+}
