995ab10f192855a00aabf3b00a17ff3fdc6ea0b9
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-08-24 14:28:59 +0700

    Create 2021-08-03-TLX-PD-3-B.cpp

---
+++ 2021-2022/2021-08-03-TLX-PD-3-B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+ 
+ 
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int A,B,Total;
+    cin >> A >> B;
+    Total = A+B;
+    cout << Total << endl;
+}
