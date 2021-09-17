b50d45f7cf581be1ca92eed699c2379ad1cdc32f
webdesain16 <77285988+webdesain16@users.noreply.github.com>
2021-09-17 14:29:25 +0700

    Create 2021-08-27-TLX-PD-5-D.cpp

---
+++ 2021-2022/2021-08-27-TLX-PD-5-D.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    //positif
+    if (N>0)
+    {
+        cout << "positif" << endl;
+    }
+    else if (N<0)
+    {
+        cout << "negatif" << endl;
+    }
+    else
+    {
+        cout << "nol" << endl;
+    }
+}
