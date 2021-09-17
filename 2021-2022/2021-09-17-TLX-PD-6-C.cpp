0e5fae14ab6f9e3eac9df161f0584d8a09fffd28
Elrond <59683978+ElrondRafsaAdjani@users.noreply.github.com>
2021-09-17 14:23:53 +0700

    Create 2021-09-17-TLX-PD-6-C.cpp

---
+++ 2021-2022/2021-09-17-TLX-PD-6-C.cpp
@@ -0,0 +1,13 @@
+// problem C - perulangan
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,hasil=0;
+	
+	while(cin >> a){
+		hasil = hasil + a;
+	}
+	cout << hasil << endl;
+}
