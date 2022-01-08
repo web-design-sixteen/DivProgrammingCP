6c9eb175bf5245c62e3077c4cc66fe227713953b
Ainun Nadhifah Syamsiyah <58164571+ainunns@users.noreply.github.com>
2022-01-08 21:48:11 +0700

    Create 2022-01-08-1-Review_Array.cpp

---
+++ Lomba CP/ILPC 2022 Preparation/2022-01-08-1-Review_Array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int arr[105]; // arr[0] sampe arr[99] -> satu dimensi
+	// ukuran array antara 0-N
+
+	// arr[0] sampe arr[100] -> > 100
+
+	int a[101][101]; // dua dimensi
+	int b[101][101][101][101]; //4 dimensi/multi dimensi
+}
